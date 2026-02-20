d4b971e26d6d1bddf3cc9fd4efaafbb4dc762573
Keith A. Lewis <kal@kalx.net>
2026-02-20 15:22:22 -0500

    typo

--- xll_bootstrap.cpp
+++ xll_bootstrap.cpp
@@ -1,12 +1,12 @@
 // xll_bootstrap.cpp - bootstrap functions
-// TODO: Implment \FI.CURVE.BOOTSTRAP(instruments, prices) 
+// TODO: Implement \FI.CURVE.BOOTSTRAP(instruments, prices) 
 /*
 handle<curve::pwflat> WINAPI xll_fi_bootstrap(_FP12* pi, _FP12* pp) { ...
 	ensure(size(*pi) == size(*pp));
 	int n = size(*pi);
 	std::vector<instrument::base<>*> is(n);
 	for (int i = 0; i < n; ++i) {
-		is[i] = ??? // instrument::base<>*.
+		is[i] = ??? // instrument::base<>* from handle::get()
 	}
 	curve::bootstrap(n, is.data(), pp->array);
 }
